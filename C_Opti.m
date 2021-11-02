2bea9c5b6aff941f5d252f7c54db61057f603d69
Nilton Garcia Hilares <alanngh@math.vt.edu>
2021-11-02 19:46:56 -0400

    plots

--- C_Opti.m
+++ C_Opti.m
@@ -21,7 +21,7 @@ function [Cop,Rop] = C_Opti(A,t,x0,C0)
     %opt = optimoptions( 'OptimalityTolerance', 1e-12); 
     %opt.StepTolerance = 1e-12;
     %opt = optimset(opt, 'Algorithm','active-set');
-    opt = optimset(opt, 'Algorithm','active-set','MaxFunEvals',1000000,'Display','off'); %% TO DO: increase tol  
+    opt = optimset(opt, 'Algorithm','active-set','MaxFunEvals',10000000,'Display','off'); %% TO DO: increase tol  
     fprintf('\n runinng fminunc')
     c_opt = fminunc(@(x) f(x,A,t,x0),c0,opt);              
     fprintf('\n getting matrices obtained')
@@ -29,8 +29,8 @@ function [Cop,Rop] = C_Opti(A,t,x0,C0)
     
     for i = 1:(3*n)        
         fprintf('\n Iterating solution %d for fmin = %e' ,i,fmin)
-        c_opt = fminunc(@(x) f(x,A,t,x0), c_opt+0.5*rand(size(c_opt)),opt);               
-        %c_opt = fminunc(@(x) f(x,A,t,x0), c_opt,opt);               
+        %c_opt = fminunc(@(x) f(x,A,t,x0), c_opt+0.5*rand(size(c_opt)),opt);               
+        c_opt = fminunc(@(x) f(x,A,t,x0), c_opt,opt);               
         [f0,C0,R0] = f(c_opt,A,t,x0);
         if (f0 < fmin)
             fprintf('\n better optimal founded fmin = %e ...',f0)
