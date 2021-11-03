98244be49b508f0a7d7b60edd46eca8559112ee2
Nilton Garcia Hilares <alanngh@math.vt.edu>
2021-11-03 15:46:17 -0400

    plots

--- C_Opti.m
+++ C_Opti.m
@@ -42,9 +42,9 @@ function [Cop,Rop] = C_Opti(A,t,x0,C0)
 
    for i = 1:50
         fprintf('\n Iterating solution %d for fmin = %e' ,i,fmin)
-        c_opt1 = fminunc(@(x) f(x,A,t,x0), c_opt +  0.1*rand(size(c_opt)),opt);
+        c_opt = fminunc(@(x) f(x,A,t,x0), c_opt +  0.5*rand(size(c_opt)),opt);
         %c_opt = fminunc(@(x) f(x,A,t,x0), c_opt,opt);
-        [f0,C0,R0] = f(c_opt1,A,t,x0);
+        [f0,C0,R0] = f(c_opt,A,t,x0);
         if (f0 < fmin)
             fprintf('\n better optimal founded fmin = %e ...',f0)
 	    %c_opt = c_opt1;  % i wasn't updating this one
