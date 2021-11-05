4de50dee113b43ce03907e20dcfd43b0e15e4cc9
Nilton Garcia Hilares <alanngh@math.vt.edu>
2021-11-05 11:12:46 -0400

    plot32

--- testbash.m
+++ testbash.m
@@ -3,9 +3,9 @@ clc, clear all, close all,
 fprintf('\n testing bash job..')
 
 initial_time    = 0
-step 		= 0.1 
-final_time    	= 2
-example_butter(2,initial_time,final_time,step);
+step 		= 2 
+final_time    	= 30
+example_butter(32,initial_time,final_time,step);
 
 
 
