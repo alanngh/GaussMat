45a48e2c724ea1fed06bc9ee431f80d41b9c6cc6
Nilton Garcia Hilares <alanngh@math.vt.edu>
2021-11-05 15:03:59 -0400

    new plots

--- testbash.m
+++ testbash.m
@@ -3,16 +3,11 @@ clc, clear all, close all,
 fprintf('\n testing bash job..')
 
 initial_time    = 0
-<<<<<<< HEAD
-step 		    = 1 
-final_time    	= 10
-n               = 4
-example_butter(n,initial_time,final_time,step);
-=======
-step 		= 2 
+step            = 2 
 final_time    	= 30
-example_butter(32,initial_time,final_time,step);
->>>>>>> 4de50dee113b43ce03907e20dcfd43b0e15e4cc9
 
+for n  = 4:1:16 
+	example_butter(n,initial_time,final_time,step);
+end
 
 
