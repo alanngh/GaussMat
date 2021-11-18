538df8d79f060baf5f41bd8a973b4080ccaee545
alanngh <31671843+alanngh@users.noreply.github.com>
2021-11-18 16:56:19 -0500

    Add files via upload

---
+++ Power_transient.m
@@ -0,0 +1,42 @@
+clc, clear all, close all
+
+
+load('dynamicdata2.mat');
+
+[m,n]=  size(M);
+
+I = eye(n);
+Z = zeros(n);
+
+%% i'm calling K to matrix D in my pdf...
+
+
+
+E = [I,Z;Z,M];
+A = [Z,I;-L,-D];
+B = [Z ; I];
+C = [I,Z];
+
+[n,mm] = size(B)
+[pp,n] = size(C)
+
+At = E\A;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+i= 1;
+t = 0:0.1:30;
+N = length(t);
+y = zeros(N,1);
+
+for i = 1:N
+    y(i) = norm(expm(At*t(i)));
+    i = i + 1
+end
+
+  AA1.y = t;
+  AA1.y = y;
+  save('ExpoBnd', '-struct', 'AA1');  
+
+
+
